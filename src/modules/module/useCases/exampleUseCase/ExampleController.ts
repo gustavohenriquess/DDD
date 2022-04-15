0cef94f390706e350b615f6198a43ef08592a954
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-04-15 19:39:59 -0300

    feat: include request type

--- src/modules/module/useCases/exampleUseCase/ExampleController.ts
+++ src/modules/module/useCases/exampleUseCase/ExampleController.ts
@@ -1,8 +1,12 @@
 import { Controller } from "@core/infra/Controller";
 import { HttpResponse } from "@core/infra/HttpResponse";
 
-export class ExampleUseCase implements Controller {
-	async handle(request: any): Promise<HttpResponse>{
+export class ExampleController implements Controller {
+	async handle(request: RequestData): Promise<HttpResponse>{
 		throw new Error('Method not implemented')
 	};
+}
+
+interface RequestData {
+	
 }
