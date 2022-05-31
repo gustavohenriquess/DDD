aa5d6d77ee81c4a2f9838c9a33b593c568339779
Gustavo Henrique <gustavo._henrique@hotmail.com>
2022-05-30 22:48:16 -0300

    feat: added memory status and added Either

--- src/modules/core/useCases/ReturnStartFile/ReturnStartFileUseCase.ts
+++ src/modules/core/useCases/ReturnStartFile/ReturnStartFileUseCase.ts
@@ -1,17 +1,32 @@
 import fs from 'fs';
-import { HttpResponse, ok } from '@core/infra/HttpResponse';
+import os from 'os';
+import { Either, right } from '@core/logic/Either';
 
 type FileType = 'utf-8';
 
 export class ReturnStartFileUseCase {
-  async execute(filePath: string, fileType: FileType): Promise<HttpResponse> {
+  execute(filePath: string, fileType: FileType): Either<Error, object> {
     if (!fs.existsSync(filePath)) {
       throw new Error('The files ${props.filePath} was not found');
     }
 
-    const file = fs.readFileSync(filePath, fileType);
-    const obj = JSON.parse(file);
+    const totalMemory = os.totalmem() / (1024 * 1024 * 1024);
+    const freeMemory = os.freemem() / (1024 * 1024 * 1024);
+    const usageMemory = process.memoryUsage.rss() / (1024 * 1024);
 
-    return ok(obj);
+    const file = fs.readFileSync(filePath, fileType);
+    const obj = {
+      uptime: process.uptime(),
+      processMemoryUsage: `${this.precision(usageMemory)} Mb`,
+      memoryTotal: `${this.precision(totalMemory)} Gb`,
+      memoryFree: `${this.precision(freeMemory)} Gb`,
+      ...JSON.parse(file),
+    };
+
+    return right(obj);
+  }
+
+  private precision(num: number): number {
+    return parseFloat(num.toFixed(2));
   }
 }
